25d85809390a59cc7dbc0af75b725e36376a5d6f
techinfoguie-png <techinfoguie@gmail.com>
2025-08-28 18:27:30 +1100

    Update main.py

--- app/src/main.py
+++ app/src/main.py
@@ -13,3 +13,20 @@ async def main():
 if __name__ == "__main__":
     import asyncio
     asyncio.run(main())
+"""
+#or src/main.py
+from telethon.sync import TelegramClient
+from config import TELEGRAM_API_ID, TELEGRAM_API_HASH, BOT_TOKEN
+from handlers import handle_new_message
+from parsers import parse_groups
+
+client = TelegramClient('session_name', TELEGRAM_API_ID, TELEGRAM_API_HASH)
+
+async def main():
+    await client.start(BOT_TOKEN)
+    await parse_groups(client)
+    await client.run_until_disconnected()
+
+if __name__ == "__main__":
+    client.loop.run_until_complete(main())
+"""
